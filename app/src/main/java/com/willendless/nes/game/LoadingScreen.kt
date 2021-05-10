d456e35953cf9ae80e98f3cb94e6e29a655bbf36
Jiarui Li <jiaruili@berkeley.edu>
2021-05-10 16:14:25 +0800

    fix: update LoadingScreen comments

--- app/src/main/java/com/willendless/nes/game/LoadingScreen.kt
+++ app/src/main/java/com/willendless/nes/game/LoadingScreen.kt
@@ -5,13 +5,13 @@ import com.willendless.nes.framework.Game
 import com.willendless.nes.framework.Screen
 import com.willendless.nes.game.mock.SnakeMainScreen
 
-// This class mainly initializes game assets and settings.
-// Then we will transit to the MainScreen based on game name.
+// This class mainly initializes game assets and CPU.
+// Then we will transit to the MainScreen according to game name.
+// Only update method is used.
 class LoadingScreen(game: Game): Screen(game) {
     override fun update(deltaTime: Float) {
         // todo(ljr): initialize game assets and settings
         // todo(ljr): dispatch according to game name
-        CPU.
         game.setScreen(SnakeMainScreen(game))
     }
 
