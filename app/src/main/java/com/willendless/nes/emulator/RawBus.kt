95c4e7edba624d7fdf019fa235f77ce98e8219fe
Qingyao Sun <sunqingyao19970825@icloud.com>
2021-05-15 17:41:36 +0800

    feat: adapt Klaus Dormann's test suite

---
+++ app/src/main/java/com/willendless/nes/emulator/RawBus.kt
@@ -0,0 +1,21 @@
+package com.willendless.nes.emulator
+
+@ExperimentalUnsignedTypes
+@ExperimentalStdlibApi
+object RawBus : Mem {
+    var mem = UByteArray(0x10000)   // 64KiB
+
+    override fun readUByte(addr: UShort): UByte = mem[addr.toInt()]
+
+    override fun writeUByte(addr: UShort, data: UByte) {
+        mem[addr.toInt()] = data
+    }
+
+    override fun populate(source: UByteArray, offset: Int) {
+        source.copyInto(mem, offset)
+    }
+
+    override fun clear() {
+        mem.fill(0x0u)
+    }
+}
