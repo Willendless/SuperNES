cc04644f8297a1de9e1e7a557b8f49835b819d4a
Jiarui Li <jiaruili@berkeley.edu>
2021-05-15 19:14:22 +0800

    fix: pass regression tests

--- app/src/test/java/com/willendless/nes/CpuTest.kt
+++ app/src/test/java/com/willendless/nes/CpuTest.kt
@@ -10,6 +10,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.After
 import org.junit.Test
+import org.junit.rules.ExpectedException
 
 @ExperimentalStdlibApi
 @ExperimentalUnsignedTypes
@@ -22,7 +23,7 @@ class CPUTest {
     }
 
     @After fun tearDown() {
-        cpu.switchBus(Bus)
+        cpu.switchBus(cpu.bus)
     }
 
     private fun loadAndRun(program: UByteArray, offset: Int = 0x500) {
@@ -34,7 +35,8 @@ class CPUTest {
     @Test fun test_rom() {
         val testRom = java.io.File("src/main/assets/testRom/nestest.nes")
         val program = testRom.readBytes().toUByteArray()
-        Bus.populate(program, 0xc000)
+        cpu.switchBus(Bus)
+        cpu.bus.populate(program, 0xc000)
         CPU.reset(0xc000u)
         CPU.run()
     }
@@ -54,58 +56,58 @@ class CPUTest {
     }
 
     @Test fun test_lda_zero_page() {
-        Bus.writeUByte(0x00u, 0x01u)
+        cpu.bus.writeUByte(0x00u, 0x01u)
         val program = ubyteArrayOf(0xa5u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(1.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_zero_page_x() {
-        Bus.writeUByte(0x01u, 0x01u)
+        cpu.bus.writeUByte(0x01u, 0x01u)
         val program = ubyteArrayOf(0xa2u, 0x01u, 0xb5u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(1.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_zero_page_x_overflow() {
-        Bus.writeUByte(0x00u, 0x02u)
+        cpu.bus.writeUByte(0x00u, 0x02u)
         val program = ubyteArrayOf(0xa2u, 0xffu, 0xb5u, 0x01u, 0x00u)
         loadAndRun(program)
         assertEquals(2.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_absolute() {
-        Bus.writeUShort(0x00u, 0x0008u)
+        cpu.bus.writeUShort(0x00u, 0x0008u)
         val program = ubyteArrayOf(0xadu, 0x00u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(8.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_absolute_x() {
-        Bus.writeUShort(0x10u, 0x0009u)
+        cpu.bus.writeUShort(0x10u, 0x0009u)
         val program = ubyteArrayOf(0xa2u, 0x10u, 0xbdu, 0x00u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(9.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_absolute_y() {
-        Bus.writeUShort(0x10u, 0x0009u)
+        cpu.bus.writeUShort(0x10u, 0x0009u)
         val program = ubyteArrayOf(0xa0u, 0x10u, 0xb9u, 0x00u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(9.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_indirect_x() {
-        Bus.writeUShort(0x00u, 0x0010u)
-        Bus.writeUShort(0x10u, 0x0010u)
+        cpu.bus.writeUShort(0x00u, 0x0010u)
+        cpu.bus.writeUShort(0x10u, 0x0010u)
         val program = ubyteArrayOf(0xa2u, 0x01u, 0xa1u, 0xffu, 0x00u)
         loadAndRun(program)
         assertEquals(0x10.toUByte(), cpu.a)
     }
 
     @Test fun test_lda_indirect_y() {
-        Bus.writeUShort(0xBBu, 0x1122u)
-        Bus.writeUShort(0x1124u, 0x00AAu)
+        cpu.bus.writeUShort(0xBBu, 0x1122u)
+        cpu.bus.writeUShort(0x1124u, 0x00AAu)
         val program = ubyteArrayOf(0xa0u, 0x02u, 0xb1u, 0xbbu, 0x00u)
         loadAndRun(program)
         assertEquals(0xaa.toUByte(), cpu.a)
@@ -114,19 +116,19 @@ class CPUTest {
     @Test fun test_sta_zero_page() {
         val program = ubyteArrayOf(0xa9u, 0x02u, 0x85u, 0x01u, 0x00u)
         loadAndRun(program)
-        assertEquals(0x02.toUByte(), Bus.readUByte(0x01u))
+        assertEquals(0x02.toUByte(), cpu.bus.readUByte(0x01u))
     }
 
     @Test fun test_stx_zero_page() {
         val program = ubyteArrayOf(0xa2u, 0x02u, 0x86u, 0x01u, 0x00u)
         loadAndRun(program)
-        assertEquals(0x02.toUByte(), Bus.readUByte(0x01u))
+        assertEquals(0x02.toUByte(), cpu.bus.readUByte(0x01u))
     }
 
     @Test fun test_sty_zero_page() {
         val program = ubyteArrayOf(0xa0u, 0x02u, 0x84u, 0x01u, 0x00u)
         loadAndRun(program)
-        assertEquals(0x02.toUByte(), Bus.readUByte(0x01u))
+        assertEquals(0x02.toUByte(), cpu.bus.readUByte(0x01u))
     }
 
     @Test fun test_tax() {
@@ -167,7 +169,7 @@ class CPUTest {
     }
 
     @Test fun test_and() {
-        Bus.writeUByte(0x00u, 0x01u)
+        cpu.bus.writeUByte(0x00u, 0x01u)
         val program = ubyteArrayOf(0xa9u, 0xffu, 0x25u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(1.toUByte(), cpu.a)
@@ -176,7 +178,7 @@ class CPUTest {
     }
 
     @Test fun test_ora() {
-        Bus.writeUByte(0x00u, 0x01u)
+        cpu.bus.writeUByte(0x00u, 0x01u)
         val program = ubyteArrayOf(0xa9u, 0xf0u, 0x05u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(0xf1.toUByte(), cpu.a)
@@ -185,7 +187,7 @@ class CPUTest {
     }
 
     @Test fun test_eor() {
-        Bus.writeUByte(0x00u, 0x01u)
+        cpu.bus.writeUByte(0x00u, 0x01u)
         val program = ubyteArrayOf(0xa9u, 0xf0u, 0x45u, 0x00u, 0x00u)
         loadAndRun(program)
         assertEquals(0xf1.toUByte(), cpu.a)
