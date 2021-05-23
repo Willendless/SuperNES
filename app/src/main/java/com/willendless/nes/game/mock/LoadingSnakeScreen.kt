1bd1dc7609728dd5ccd5cc6e03d66ea1fa7e4d0b
Jiarui Li <jiaruili@berkeley.edu>
2021-05-23 12:49:31 +0800

    fix(snake): make snake move again

--- app/src/main/java/com/willendless/nes/game/mock/LoadingSnakeScreen.kt
+++ app/src/main/java/com/willendless/nes/game/mock/LoadingSnakeScreen.kt
@@ -1,5 +1,7 @@
 package com.willendless.nes.game.mock
 
+import com.willendless.nes.emulator.NESBus
+import com.willendless.nes.emulator.Rom
 import com.willendless.nes.emulator.cpu.CPU
 import com.willendless.nes.framework.Game
 import com.willendless.nes.framework.Screen
@@ -12,7 +14,8 @@ import com.willendless.nes.framework.Screen
 class LoadingSnakeScreen(game: Game): Screen(game) {
     override fun update(deltaTime: Float) {
         val program = game.getFileIO().readAsset("testGames/snake.nes").readBytes().toUByteArray()
-        CPU.load(program, 0x8000)
+        val rom = Rom(program)
+        NESBus.init(rom)
         CPU.reset()
         game.setScreen(SnakeMainScreen(game))
     }
